3d118e2b9c9d62c064af67b874d3c094fb621537
Hossein Mireslami <mireslami809@gmail.com>
2025-01-10 23:24:32 +0330

    added basic and login files

--- my_project/main.c
+++ my_project/main.c
@@ -3,12 +3,35 @@
 #include<stdlib.h>
 #include<ncurses.h>
 
+#include "basic_loads.c"
+#include "login.c"
+
 typedef struct
 {
     int x,y;
-} Point;
+} Loc;
 
 
 int main(){
-    
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);
+    // noecho();
+
+    printw("Hello!\n");
+    if(!has_colors()){
+        endwin();
+        printf("Sorry! Your Terminal Doesn't support colors!\n");
+        return 0;
+    }
+    refresh();
+    int count = 0;
+    while(1){
+        mvprintw(0,0,"hey! count is %d\n", count);
+        char c = getch();
+        count++;
+        if(c == 'q') break;
+        refresh();
+    }
+    endwin();
 }
