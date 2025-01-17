0194c5a93f9b3e237e5ce522131f5bc840e53ac5
Hossein Mireslami <mireslami809@gmail.com>
2025-01-17 11:21:22 +0330

    custom color debugged

--- my_project/basic_loads.h
+++ my_project/basic_loads.h
@@ -7,26 +7,35 @@
 
 // New COLORS
 #define COLOR_PURPLE 10
-#define COLOR_CUSTOM_RED 11
-#define COLOR_CUSTOM_CYAN 12
+#define CUSTOM_RED 11
+#define CUSTOM_CYAN 12
+#define CUSTOM_GREEN 13
 
 // New PAIRS
 #define BG_LOGIN 0
 #define MESSAGE_COLOR 1
 #define BTN_DEFAULT 2
 #define BTN_SELECTED 3
-#define TEXT_COLOR 4
+#define HEADER_COLOR 4
+#define TEXT_COLOR 5
+
+void add_color_rgb(int id, int r, int g, int b){
+    start_color();
+    init_color(id, r*1000/255, g*1000/255, b*1000/255);
+}
 
 void game_initalize(){
     start_color();
-    init_color(COLOR_PURPLE, 71, 50, 122);
-    init_color(COLOR_CUSTOM_RED, 203, 35, 44);
-    init_color(COLOR_CUSTOM_CYAN, 151, 176, 243);
+    add_color_rgb(COLOR_PURPLE, 71, 50, 122);
+    add_color_rgb(CUSTOM_RED, 203, 35, 44);
+    add_color_rgb(CUSTOM_GREEN, 93, 255, 101);
+    add_color_rgb(CUSTOM_CYAN, 151, 176, 243);
 
     init_pair(MESSAGE_COLOR, COLOR_RED, COLOR_BLACK);
     init_pair(BTN_DEFAULT, COLOR_CYAN, COLOR_BLACK);
     init_pair(BTN_SELECTED,COLOR_BLACK , COLOR_CYAN);
     init_pair(BG_LOGIN, COLOR_PURPLE, COLOR_PURPLE);
+    init_pair(HEADER_COLOR, CUSTOM_CYAN, COLOR_BLACK);
     init_pair(TEXT_COLOR, COLOR_WHITE, COLOR_BLACK);
 }
 
